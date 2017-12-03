ce26931d914103343b58dc39e246e4a6ca0b1753
Mathias Petersen <mathias.petersen95@gmail.com>
2017-12-03 20:48:35 +0100

    Add generic abstraction to IApplyEvent interface

--- src/Eventure/Domain/Aggregate/IApplyEvent.cs
+++ src/Eventure/Domain/Aggregate/IApplyEvent.cs
@@ -1,8 +1,17 @@
-﻿using Eventure.Domain.DomainEvents;
+﻿using System;
+using Eventure.Domain.DomainEvents;
 
 namespace Eventure.Domain.Aggregate
 {
-    public interface IApplyEvent<in TEvent> where TEvent: IEvent
+    public interface IApplyEvent<in TEvent> : IApplyEvent<TEvent, Guid, Guid> where TEvent : IEvent<Guid, Guid>
+    {
+    }
+    
+    
+    public interface IApplyEvent<in TEvent, TEventId, TAggregateId> 
+        where TEvent: IEvent<TEventId, TAggregateId> 
+        where TEventId : IComparable, IComparable<TEventId>, IEquatable<TEventId> 
+        where TAggregateId : IComparable, IComparable<TAggregateId>, IEquatable<TAggregateId>
     {
         void Apply(TEvent @event);
     }
